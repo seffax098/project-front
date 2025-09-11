72c45a671fa6c8f227aacf6bfa3a9d019b52f8ef
seffax098 <dkutilkin@gmail.com>
2025-09-11 20:53:22 +0300

    Practical 3-4

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ form?.addEventListener('submit', (e) => {
         const email = form.elements.email;
         if (email?.validity.typeMismatch) {
             email.setCustomValidity('Введите корректный e-mail, например name@example.com');
- }
+        }
         form.reportValidity(); // показать браузерные подсказки
         // A11y: подсветка проблемных полей
         [...form.elements].forEach(el => {
@@ -44,16 +44,16 @@ form?.addEventListener('submit', (e) => {
 
 const phone = document.getElementById('phone');
 phone?.addEventListener('input', () => {
- const digits = phone.value.replace(/\D/g,'').slice(0,11); // до 11 цифр
- const d = digits.replace(/^8/, '7'); // нормализуем 8 → 7
- const parts = [];
- if (d.length > 0) parts.push('+7');
- if (d.length > 1) parts.push(' (' + d.slice(1,4));
- if (d.length >= 4) parts[parts.length - 1] += ')';
- if (d.length >= 5) parts.push(' ' + d.slice(4,7));
- if (d.length >= 8) parts.push('-' + d.slice(7,9));
- if (d.length >= 10) parts.push('-' + d.slice(9,11));
- phone.value = parts.join('');
+    const digits = phone.value.replace(/\D/g, '').slice(0, 11); // до 11 цифр
+    const d = digits.replace(/^8/, '7'); // нормализуем 8 → 7
+    const parts = [];
+    if (d.length > 0) parts.push('+7');
+    if (d.length > 1) parts.push(' (' + d.slice(1, 4));
+    if (d.length >= 4) parts[parts.length - 1] += ')';
+    if (d.length >= 5) parts.push(' ' + d.slice(4, 7));
+    if (d.length >= 8) parts.push('-' + d.slice(7, 9));
+    if (d.length >= 10) parts.push('-' + d.slice(9, 11));
+    phone.value = parts.join('');
 });
 // Строгая проверка (если задаёте pattern из JS):
 phone?.setAttribute('pattern', '^\\+7 \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}$');
